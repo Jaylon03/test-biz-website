b0bf02c8264f95ea05f8df890b4b287c2da81522
Jaylon Nelson <jaylonnell03@gmail.com>
2024-08-25 02:44:39 -0500

    changes

--- javascript.js
+++ javascript.js
@@ -139,34 +139,3 @@ function toggleMobileMenu(menu) {
     document.querySelector('.mobile-menu').classList.toggle('open');
 }
 
-
-import axios from 'axios' 
-
-const api_key = 'ebe16e9b-19bb-437b-bcac-0e25f3e98a6f' 
-const assistant_id = '4beb5580-c40a-4e20-a550-03efdd0f7360' 
-let session_id;
-
-// Create a new chat session 
-const chatSession = await axios.post(
-   'https://agentivehub.com/api/chat',
- {
-    "api_key": api_key,
-     "assistant_id": "ff9a4dc0-0126-492a-bfb3-a354e0c136c2"
-    }
-)
-
-session_id = chatSession.data.session_id;
-
- // Send a message to the chat session 
-const chatReponse = {
- api_key,
- session_id,
- assistant_id,
- messages:[{ role: 'user',  content: 'Say Hello!' }]
- };
-
-const chat = await axios.post(
-   'https://agentivehub.com/api/chat',
- chatReponse
- );
-    
